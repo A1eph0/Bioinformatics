6415ddb02475bd09a597cc9cfd633ee0f1e35015
Hrishi Narayanan <68889529+hr1sh1-coding@users.noreply.github.com>
2021-09-30 16:46:45 +0530

    programming assignment 4

---
+++ Programming Assignment-2/README.md
@@ -0,0 +1,8 @@
+# Programming Assignment-2
+
+* Execute the program by running `python3 main.py` on the terminal.
+* Program is cabale of handling DP for both global and nucleotide alignment for given nucleotide/protein sequences.
+* Gap penalty is set at 5 and is levied in a linear manner.
+* Insulin protien from Octodon degus (accession no.: AAA40590.1) has been compared with insulin protein from Camelus dromedarius (accession no.: KAB1251309.1).
+* The scores for global alignment obtained is 308 and that for local alignment obtained is 318, which match with the output obtained from EMBOSS needle and water respectively.
+* EMBOSS needle and water output files are provided. Alternatively, they can be obtained [here](https://www.ebi.ac.uk/Tools/services/web/toolresult.ebi?jobId=emboss_needle-I20210930-112122-0709-33844378-p1m) and [here](https://www.ebi.ac.uk/Tools/services/web/toolresult.ebi?jobId=emboss_water-I20210930-110232-0999-44288907-p1m) respectively. 
---
+++ Programming Assignment-2/main.py
@@ -0,0 +1,131 @@
+import numpy as np
+import os
+import parameters as params
+
+
+# Needleman-Wunsch algorithm
+def global_alignment(seq1, seq2, scoring_matrix):
+
+    gap_penalty = -5
+
+    n = len(seq1)
+    m = len(seq2)
+
+    dp = np.zeros((n + 1, m + 1), dtype=int)
+
+    for i in range(n + 1):
+        dp[i][0] = gap_penalty * i
+    for i in range(m + 1):
+        dp[0][i] = gap_penalty * i
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            dp[i][j] = max(
+                            dp[i-1][j-1] + scoring_matrix[seq1[i-1]][seq2[j-1]], 
+                            dp[i][j-1] + gap_penalty, 
+                            dp[i-1][j] + gap_penalty
+                        )
+
+    gen_seq1 = ""
+    gen_seq2 = ""
+
+    i, j = n, m
+    while i > 0 or j > 0:
+        if i > 0 and j > 0 and dp[i-1][j-1] == dp[i][j] - scoring_matrix[seq1[i-1]][seq2[j-1]]:
+            gen_seq1 += seq1[i-1]
+            gen_seq2 += seq2[j-1]
+            i -= 1
+            j -= 1
+        elif j > 0 and dp[i][j-1] == dp[i][j] - gap_penalty:
+            gen_seq1 += '_'
+            gen_seq2 += seq2[j-1]
+            j -= 1
+        elif i > 0 and dp[i-1][j] == dp[i][j] - gap_penalty:
+            gen_seq1 += seq1[i-1]
+            gen_seq2 += '_'
+            i -= 1
+
+    gen_seq1 = gen_seq1[::-1]
+    gen_seq2 = gen_seq2[::-1]
+
+    print('\nGlobal Alignment:\n\n', 'Score:', dp[n][m], '\n\n', 'Sequence 1:', gen_seq1, '\n', 'Sequence 2:', gen_seq2, '\n', sep='')
+
+
+# Smith-Waterman algorithm
+def local_alignment(seq1, seq2, scoring_matrix):
+
+    gap_penalty = -5
+    
+    n = len(seq1)
+    m = len(seq2)
+    
+    dp = np.zeros((n + 1, m + 1), dtype=int)
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            dp[i][j] = max(
+                            dp[i-1][j-1] + scoring_matrix[seq1[i-1]][seq2[j-1]], 
+                            dp[i][j-1] + gap_penalty, 
+                            dp[i-1][j] + gap_penalty, 
+                            0
+                        )
+    
+    gen_seq1 = ""
+    gen_seq2 = ""
+    
+    i, j = 0, 0
+    
+    score = 0
+    for k in range(n+1):
+        for l in range(m+1):
+            if score <= dp[k][l]:
+                i, j = k, l
+                score = dp[k][l]
+
+    while (i > 0 or j > 0) and dp[i][j] > 0:
+        if i > 0 and j > 0 and dp[i-1][j-1] == dp[i][j] - scoring_matrix[seq1[i-1]][seq2[j-1]]:
+            gen_seq1 += seq1[i-1]
+            gen_seq2 += seq2[j-1]
+            i -= 1
+            j -= 1
+        elif j > 0 and dp[i][j-1] == dp[i][j] - gap_penalty:
+            gen_seq1 += '_'
+            gen_seq2 += seq2[j-1]
+            j -= 1
+        elif i > 0 and dp[i-1][j] == dp[i][j] - gap_penalty:
+            gen_seq1 += seq1[i-1]
+            gen_seq2 += '_'
+            i -= 1
+    
+    gen_seq1 = gen_seq1[::-1]
+    gen_seq2 = gen_seq2[::-1]
+    
+    print('\nLocal Alignment:\n\n', 'Score:', score, '\n\n', 'Sequence 1:', gen_seq1, '\n', 'Sequence 2:', gen_seq2, '\n', sep='')
+
+
+# Main loop
+if __name__ == "__main__":
+
+
+    seq_type = input("For nucleotide, press 0 and for protein, press 1:\n")
+    align_type = input("\nFor global alignment, press 0 and for local alignment, press 1:\n")
+    seq1_path = input("\nEnter path to first sequence:\n")
+    seq2_path = input("\nEnter path to second sequence:\n")
+
+    if os.path.isfile(seq1_path) and os.path.isfile(seq2_path):
+        with open(seq1_path, 'r') as file:
+            seq1 = file.read().replace('\n', '')
+        with open(seq2_path, 'r') as file:
+            seq2 = file.read().replace('\n', '')
+
+        scoring_matrix = params.BLAST_GAPPED_SCORING
+        if seq_type == "1":
+            scoring_matrix = params.BLOSUM62
+
+        if align_type == "1":
+            local_alignment(seq1, seq2, scoring_matrix)
+        else:
+            global_alignment(seq1, seq2, scoring_matrix)
+    
+    else:
+        print("Invalid input path")
